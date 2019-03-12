9ecb1ab6c92adb5bde4339d5a7a98a1338b2b4d9
MikleBe <mikle.pastorbe@gmail.com>
2019-03-12 20:36:53 +0300

    good work

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let result = 0;
+    const arr = [];
+    for(let i = 0; i < preferences.length; i++) {
+
+        const personA = preferences[i];
+        const personB = preferences[personA - 1];
+        const personC = preferences[personB - 1];
+        if (personC - 1 === i && !arr.includes(personA) && personA != personC) {
+            result++;
+            arr.push(personA, personB, personC);
+        }
+    }
+    return result;
 };
